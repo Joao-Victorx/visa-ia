8a1be16a73082f06cd0c3e22c315236030532298
Joao-Victorx <joaovictormarcondes28@gmail.com>
2026-05-23 20:32:06 -0300

    feat: Backend funcional com ocr e ia bert-pt integrado

--- frameworks/api/main.py
+++ frameworks/api/main.py
@@ -1,18 +1,20 @@
 from fastapi import FastAPI, UploadFile, File
-# Ajuste as importações para o local correto das suas pastas na raiz:
 from adapters.gateways.tesseract_ocr_gateway import TesseractOCRGateway
 from use_cases.analisar_documento import AnalisarDocumentoUseCase
+from frameworks.ia.bert_ia_service import BertIAService  # Import novo
 
 app = FastAPI(title="VISA Londrina IA")
+
+# Inicializa a IA uma vez só (fora da rota) para não lentificar o upload
+ia_service = BertIAService()
 
 
 @app.post("/documentos/analisar")
 async def analisar_doc(file: UploadFile = File(...)):
-    # Inicializa o motor de OCR
     ocr_gtw = TesseractOCRGateway()
 
-    # Inicializa o caso de uso (Passando None na IA por enquanto)
-    use_case = AnalisarDocumentoUseCase(ocr_gtw, ia_service=None)
+    # Agora passamos o serviço de IA real!
+    use_case = AnalisarDocumentoUseCase(ocr_gtw, ia_service=ia_service)
 
     conteudo_arquivo = await file.read()
     resultado = use_case.executar(conteudo_arquivo, file.filename)
